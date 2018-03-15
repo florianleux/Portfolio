c4a7c7d1ad6ad2811bcb59bf340641e1f71f3b7e
Florian Leux <florian.leux@gmail.com>
2018-03-15 09:57:47 +0100

    Mise en place PHP et dossier SB

--- js/vue.js
+++ js/vue.js
@@ -4,12 +4,21 @@ window.onload=function() {
         template: '<li>Ceci est une liste</li>'
     })
 
+    $.ajax("sketchbooks/titles.php")
+        .done(function (response){
+            console.log("coucou");
+            console.log(response);
+        })
+    ;
+
+
     new Vue({
         el: '#app',
         data: {
             message: 'Hello Vue.js!'
         }
     });
+    console.log("couou");
 
 
 
---
+++ sketchbooks/titles.php
@@ -0,0 +1,18 @@
+<?php
+
+$titles = array(
+    array(
+        'title' => 'Mon sketchbook number one',
+        'count' => 12
+    ),
+    array(
+        'title' => 'Mon sketchbook number two',
+        'count' => 22
+    ),
+    array(
+        'title' => 'Mon sketchbook number three',
+        'count' => 22
+    )
+);
+
+echo json_encode($titles);
