b1afaed6e0520a579dec96a15e5a07de5abf84af
Florian Leux <florian.leux@gmail.com>
2018-05-21 18:31:50 +0200

    Premiere mise en ligne

--- sketchbooks/titles.php
+++ sketchbooks/titles.php
@@ -4,48 +4,8 @@
 
 $titles = array(
     array(
-        'title' => 'Travelers Railway',
-        'id' => '11',
-    ),
-    array(
-        'title' => 'Dracula A Loup-Garou',
-        'id' => '10',
-    ),
-    array(
-        'title' => 'Kame Burger',
-        'id' => '9',
-    ),
-    array(
-        'title' => 'Cuties',
-        'id' => '8',
-    ),
-    array(
-        'title' => 'Iguana Bay',
-        'id' => '7',
-    ),
-    array(
-        'title' => "Mickey's love",
-        'id' => '6',
-    ),
-    array(
-        'title' => 'Paxtown Girl',
-        'id' => '5',
-    ),
-    array(
         'title' => 'Auberge de la Patchole',
         'id' => '1',
-    ),
-    array(
-        'title' => 'Fox',
-        'id' => '2'
-    ),
-    array(
-        'title' => 'Rondoudou & Parker',
-        'id' => '3'
-    ),
-    array(
-        'title' => 'H',
-        'id' => '4'
     )
 );
 
